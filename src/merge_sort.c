35f8fa728780eab08d6f1abdafa418588edea524
Jesi <jesi.nieves@gmail.com>
2018-01-28 20:47:10 -0400

    Debug

--- src/merge_sort.c
+++ src/merge_sort.c
@@ -18,14 +18,16 @@ int main(int argc, char* argv[]);
 
 
 void run_omp(int a[], int size, int temp[], int threads) {
+  printf("Entered run_omp\n");
     // Enable nested parallelism, if available
   omp_set_nested(1);
     // Parallel mergesort
   mergesort_parallel(a, size, temp, threads);
-
+  printf("Exit run_omp\n");
 }
 
 void merge(int a[], int size, int temp[]) {
+  printf("Entered merge\n");
   int i1 = 0;
   int i2 = size/2;
   int tempi = 0;
@@ -54,7 +56,7 @@ void merge(int a[], int size, int temp[]) {
 }
 
 void mergesort_serial(int a[], int size, int temp[]) {
-
+  printf("Entered mergesort_serial\n");
   mergesort_serial(a, size/2, temp);
   mergesort_serial(a + size/2, size - size/2, temp);
     // The above call will not work properly in an OpenMP program
@@ -64,14 +66,14 @@ void mergesort_serial(int a[], int size, int temp[]) {
 
 
 void mergesort_parallel(int a[], int size, int temp[], int threads) {
-
+  printf("Entered mergesort_parallel\n");
   if ( threads == 1) {
-//        printf("Thread %d begins serial merge sort\n", omp_get_thread_num());
+    //printf("Thread %d begins serial merge sort\n", omp_get_thread_num());
     mergesort_serial(a, size, temp);
   } else if (threads > 1) {
-       #pragma omp parallel sections num_threads(2)
-   {
-//            printf("Thread %d begins recursive section\n", omp_get_thread_num());
+          #pragma omp parallel sections num_threads(2)
+          {
+            // printf("Thread %d begins recursive section\n", omp_get_thread_num());
             #pragma omp section
             { //printf("Thread %d begins recursive call\n", omp_get_thread_num());
 
@@ -80,36 +82,36 @@ void mergesort_parallel(int a[], int size, int temp[], int threads) {
             { //printf("Thread %d begins recursive call\n", omp_get_thread_num());
             mergesort_parallel(a + size/2, size - size/2, temp + size/2, threads - threads/2);}
 
-      // The above use of temp + size/2 is an essential change from the serial version  
+            // The above use of temp + size/2 is an essential change from the serial version  
           }
-     // Thread allocation is implementation dependent
-       // Some threads can execute multiple sections while others are idle 
-       // Merge the two sorted sub-arrays through temp
+        // Thread allocation is implementation dependent
+        // Some threads can execute multiple sections while others are idle 
+        // Merge the two sorted sub-arrays through temp
           merge(a, size, temp); 
         } else {
          printf("Error: %d threads\n", threads); 
          return;
        }
-     }
+}
 
-    int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
     puts("-OpenMP Recursive Mergesort-\t");
     // Check arguments
-    if ( argc != 3 ) /* argc must be 3 for proper execution! */
+    if ( argc != 2 ) /* argc must be 3 for proper execution! */
       {
         printf( "Usage: %s array-size number-of-threads\n", argv[0]);
         return 1;
       }
-  // Get arguments
+    // Get arguments
     int size = SIZE; // Array size 
     int threads = atoi(argv[1]); // Requested number of threads
-  // Check nested parallelism availability
+    // Check nested parallelism availability
     omp_set_nested(1);
     if (omp_get_nested() !=1 )
     {
       puts("Warning: Nested parallelism desired but unavailable");
     } 
-  // Check processors and threads
+    // Check processors and threads
     int processors = omp_get_num_procs(); // Available processors
     printf("Array size = %d\nProcesses = %d\nProcessors = %d\n", size, threads, processors);
     if (threads > processors) 
@@ -131,12 +133,15 @@ void mergesort_parallel(int a[], int size, int temp[], int threads) {
       printf( "Error: Could not allocate array of size %d\n", size);
       return 1;
     }
+    printf("Memory Allocated\n");
 
     int count = 0;
 
     for (int i=0; i<size; i++){
       a[i] = 0;
     }
+
+    printf("a[] initialized with zeroes\n");
 
     //Read and intialize array from file
     FILE *file;
@@ -146,19 +151,24 @@ void mergesort_parallel(int a[], int size, int temp[], int threads) {
       perror("Error opening file");
       return -1;
     }
-
+    printf("File opened\n");
+    printf("Starting to read from file\n");
     while (!feof(file) && (count < size)){
-      fscanf(file, "%d", a[count++]);
+      fscanf(file, "%d", &a[count++]);
     }
     fclose(file);
+    printf("File read\n");
 
     printf("Size: %d\n", count);
-    for(int i = 0; i < count; i++)
+    /*for(int i = 0; i < count; i++)
     {
       printf("a[%d] = %d\n", i, a[i]);
     }
+    printf("Ended printing array\n");
 
-    /*// Random array initialization
+    */
+    /*
+    // Random array initialization
     int i;
     srand(time(NULL));
     for(i=0; i<size; i++) {
@@ -166,12 +176,13 @@ void mergesort_parallel(int a[], int size, int temp[], int threads) {
     }
     */
 
-  // Sort
+    // Sort
     double start = omp_get_wtime();
+    printf("Start time %lf\n", start);
     run_omp(a, size, temp, threads);
     double end = omp_get_wtime( );
     printf("Start = %.2f\nEnd = %.2f\nElapsed = %.2f\n", start, end, end - start);
-  // Result check
+    // Result check
     for(int i=1; i<size; i++) {
       if (!(a[i-1] <= a[i])) {
         printf("Implementtion error: a[%d]=%d > a[%d]=%d\n", i-1, a[i-1], i, a[i]);
